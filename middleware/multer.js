ce211a6d53a64610e66eab822dfb3304871db89a
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-11-16 10:06:19 +0300

    task1

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: "./media",
+  destination: "./media/pic_name",
   filename: (req, file, cb) => {
     cb(null, `${+new Date()}${file.originalname}`);
   },
